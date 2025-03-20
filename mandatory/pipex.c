d853f69c0e96f95cf925badd68731c34eb6eb0f0
Reda daghouj <mohamedredadaghouj@gmail.com>
2025-03-20 09:46:38 +0000

    test something

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -6,11 +6,17 @@
 /*   By: mdaghouj <mdaghouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 02:19:13 by mdaghouj          #+#    #+#             */
-/*   Updated: 2025/03/16 02:51:05 by mdaghouj         ###   ########.fr       */
+/*   Updated: 2025/03/20 09:46:10 by mdaghouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	error_msg(void)
+{
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
 
 int	main(int argc, char **argv)
 {
@@ -18,31 +24,32 @@ int	main(int argc, char **argv)
 	int		pid;
 
 	if (pipe(pipefd) == -1)
-	{
-		perror("Error");
-		exit(EXIT_FAILURE);
-	}
+		error_msg();
 	pid = fork();
 	if (pid == -1)
+		error_msg();
+	else if (pid == 0)
 	{
-		perror("Error");
-		exit(EXIT_FAILURE);
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		char *args[] = {"ls", NULL};
+		if (execve("/bin/ls", args, NULL) == -1)
+			error_msg();	
 	}
-	if (pid == 0)
+	int pid2 = fork();
+	if (pid2 == 0)
 	{
-		char *arg[] = {"wc", "-l", NULL};
 		close(pipefd[1]);
 		dup2(pipefd[0], STDIN_FILENO);
 		close(pipefd[0]);
-		printf("Child process: Running 'wc -l'\n");
-		execve("/usr/bin/wc", arg, NULL);
+		char *args[] = {"wc", "-l", NULL};
+		if (execve("/usr/bin/wc", args, NULL) == -1)
+			error_msg();		
 	}
-	else
-	{
-		close(pipefd[0]);
-		write(pipefd[1], "line\nline\nline\n", 16);
-		printf("Parent process: Sent data to child\n");
-		close(pipefd[1]);
-	}
+	close(pipefd[1]);
+	close(pipefd[0]);
+	waitpid(pid, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (EXIT_SUCCESS);
 }
